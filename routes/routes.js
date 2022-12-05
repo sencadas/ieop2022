8f9fe3349479f8091b34b3097abe262dfd7110e1
João Sencadas <joao.sencadas.tgp2@gmail.com>
2022-12-05 17:29:36 +0000

    Novo endpoint

--- routes/routes.js
+++ routes/routes.js
@@ -12,40 +12,12 @@ const {
 const axios = require("axios");
 const { getToken } = require("../utils/utils.js");
 
-router.get("/token", async (req, res) => {
-  try {
-    const response = await axios.post(
-      "https://identity.primaverabss.com/connect/token",
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-          Connection: "keep-alive",
-        },
-      },
-      {
-        auth: {
-          username: CLIENT_ID,
-          password: CLIENT_SECRET,
-        },
-        form: {
-          grant_type: "client_credentials",
-          scope: "application",
-        },
-      }
-    );
-    console.log(response);
-    res.send(response.data);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
 router.get("/materials", async (req, res) => {
   let artigosList = [];
   const token = await getToken();
-  console.log(token);
+  //console.log(token);
   const config = {
-    headers: { Authorization: `Bearer ${token.access_token}` },
+    headers: { Authorization: `${token.token_type} ${token.access_token}` },
     "Content-Type": "application/json",
   };
   axios
@@ -54,11 +26,46 @@ router.get("/materials", async (req, res) => {
       config
     )
     .then((response) => {
-      console.log(response);
+      //console.log(response);
       response.data.forEach((livro) => {
+        artigosList.push({
+          itemKey: livro.itemKey,
+          description: livro.description,
+          stock: livro.materialsItemWarehouses[1].stockBalance,
+        });
+      });
+      res.send(artigosList);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.send({
+        status: error.status,
+        message: error.message,
+      });
+    });
+});
+
+router.get("/salesitems", async (req, res) => {
+  let artigosList = [];
+  const token = await getToken();
+
+  const config = {
+    headers: { Authorization: `Bearer ${token.access_token}` },
+    "Content-Type": "application/json",
+  };
+  axios
+    .get(`${URL}/api/${TENANT}/${ORGANIZATION}/salescore/salesitems`, config)
+    .then((response) => {
+      //console.log(response);
+      response.data.forEach((livro) => {
+        console.log(livro.priceListLines);
         artigosList.push({
           name: livro.itemKey,
           description: livro.description,
+          barcode: livro.barcode,
+          priceAmount: livro.priceListLines.length
+            ? livro.priceListLines[0].priceAmount.amount
+            : null,
         });
       });
       res.send(artigosList);
