b82dda69218bbc4d7aee670b960383b60dadbd88
João Sencadas <joao.sencadas.tgp2@gmail.com>
2022-12-23 19:40:58 +0000

    Criar faturas done

--- routes/routes.js
+++ routes/routes.js
@@ -47,6 +47,94 @@ router.get("/materials", async (req, res) => {
     });
 });
 
+router.post("/invoices", async (req, res) => {
+  const token = await getToken();
+  //console.log(token);
+  const config = {
+    headers: { Authorization: `${token.token_type} ${token.access_token}` },
+    "Content-Type": "application/json",
+  };
+  let date = new Date();
+
+  const { itemKey, description, quantity, unitPrice, emailTo } = req.body;
+
+  const body = {
+    documentType: "FA",
+    serie: "2022",
+    seriesNumber: 1,
+    company: "Default",
+    paymentTerm: "01",
+    paymentMethod: "NUM",
+    currency: "EUR",
+    documentDate: date,
+    postingDate: date,
+    emailTo: emailTo,
+    buyerCustomerParty: "INDIF",
+    buyerCustomerPartyName: "Cliente Indiferenciado",
+    //"accountingPartyName": "Sofrio, Lda.",
+    //"accountingPartyTaxId": "593362462",
+    exchangeRate: 1,
+    discount: 0,
+    loadingCountry: "PT",
+    unloadingCountry: "PT",
+    isExternal: false,
+    isManual: false,
+    isSimpleInvoice: false,
+    isWsCommunicable: false,
+    deliveryTerm: "V-VIATURA",
+    documentLines: [
+      {
+        salesItem: itemKey,
+        description: description,
+        warehouse: "PO",
+        quantity: quantity,
+        unitPrice: {
+          amount: unitPrice,
+          baseAmount: unitPrice,
+          reportingAmount: unitPrice,
+          fractionDigits: 2,
+          symbol: "€",
+        },
+        unit: "UN",
+        itemTaxSchema: "NORMAL",
+        deliveryDate: date,
+      },
+    ],
+    WTaxTotal: {
+      amount: 0,
+      baseAmount: 0,
+      reportingAmount: 0,
+      fractionDigits: 2,
+      symbol: "€",
+    },
+    TotalLiability: {
+      baseAmount: 0,
+      reportingAmount: 0,
+      fractionDigits: 2,
+      symbol: "€",
+    },
+  };
+
+  axios
+    .post(`${URL}/api/${TENANT}/${ORGANIZATION}/billing/invoices`, body, config)
+    .then((response) => {
+      console.log(response);
+      res.send({
+        status: true,
+        message: "Fatura emitida",
+        data: response.data,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.send({
+        status: false,
+        message: error.message,
+        data: error.data,
+      });
+    });
+});
+
 router.get("/salesitems", async (req, res) => {
   let artigosList = [];
   const token = await getToken();
